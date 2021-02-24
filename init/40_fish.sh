985a24e68b262dd17773392faecabe620ed21f95
Diego Lameira <diegolameira@me.com>
2021-02-24 09:46:18 -0300

    fix fish command on osx

--- init/40_fish.sh
+++ init/40_fish.sh
@@ -1,8 +1,18 @@
+# Add fish to the known shells
+sudo sh -c 'echo /usr/local/bin/fish >> /etc/shells'
+
+# Set fish as the default shell
+chsh -s /usr/local/bin/fish
 
 curl -L https://get.oh-my.fish | fish
 curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
+
 fish -c "fisher install acomagu/fish-async-prompt"
 fish -c "fisher install joseluisq/gitnow"
+
+# Add brew binaries in fish path
+fish -c "set -U fish_user_paths /usr/local/bin $fish_user_paths"
+
 # fish -c "omf reload"
 # omf install agnoster
 # omf install robbyrussell
--- init/90_osx.sh
+++ init/90_osx.sh
@@ -6,14 +6,3 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Show hidden files
 defaults write com.apple.finder AppleShowAllFiles YES
-
-# FISH
-
-# Add fish to the known shells
-sudo sh -c 'echo /usr/local/bin/fish >> /etc/shells'
-
-# Set fish as the default shell
-chsh -s /usr/local/bin/fish
-
-# Add brew binaries in fish path
-set -U fish_user_paths /usr/local/bin $fish_user_paths
